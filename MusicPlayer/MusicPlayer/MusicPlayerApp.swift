932155cb225083f3a0cb6714ade0bc1a21435656
George Kapoya <george.kapoya@fi.abb.com>
2022-09-10 21:11:06 +0300

    first commit

---
+++ MusicPlayer/MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,24 @@
+//
+// ABB Oy, Drives CONFIDENTIAL
+// ---------------------------
+//
+// ABB Oy, Drives.
+// All Rights Reserved.
+//
+// NOTICE: All information contained herein is, and remains the property of ABB Oy, Drives.
+// The intellectual and technical concepts contained herein are proprietary to ABB Oy, Drives and
+// its suppliers.
+// Dissemination of this information or reproduction of this material is strictly forbidden unless
+// prior written permission is obtained from ABB Oy, Drives.
+
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
